118fbfd0f2431fc723b35b53b07b6399ccb4d354
meakaliaG <124390595+meakaliaG@users.noreply.github.com>
2024-10-11 09:50:19 -0400

    partner B complete

--- favoriteNumbers/favoriteNumbers/Program.cs
+++ favoriteNumbers/favoriteNumbers/Program.cs
@@ -1,10 +1,55 @@
 ﻿namespace favoriteNumbers
+    /* Authors: Noah Wexler and Meakalia Gilman
+     * Class: IGM.201.01 Interac Des & Prob Sol III
+     * Due: 10.13.24
+     */
 {
     internal class Program
     {
+        //static addition function here
+        static void add(int a, int b) { }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //declare variables
+            int noahFavoiteNumber;
+            int meaFavoriteNumber;
+
+            //initialize variables
+            //REPLACE 0 WITH UR FAV NUM
+            noahFavoiteNumber = 0;
+            meaFavoriteNumber = 17;
+
+            //subtract function
+            static int subtract(int a, int b)
+            {
+                return a - b;
+            }
+
+            //finally, the program start!
+            //DELETE FOLLOWING FORWARD SLASHES
+            //Console.WriteLine("Noah's Favorite Number: " + noahFavoriteNumber);
+            Console.WriteLine("Meakalia's Favorite Number: " + meaFavoriteNumber);
+
+            //DELETE FOLLOWING FORWARD SLASHES
+            //int sum = add(noahFavoiteNumber, meaFavoriteNumber);
+            int diff = 0;
+            if (noahFavoiteNumber > meaFavoriteNumber)
+            {
+                diff = subtract(noahFavoiteNumber, meaFavoriteNumber);
+            }
+            else
+            {
+                diff = subtract(meaFavoriteNumber, noahFavoiteNumber);
+            }
+
+            //print results
+            //DELETE FOLLOWING FORWARD SLASHES
+            //Console.WriteLine("Favorite numbers sum: " + sum);
+            Console.WriteLine("Favorite numbers difference: " + diff);
+
+
         }
     }
 }
