bc80affb4b9662831456f033160ab372695a018f
meakaliaG <124390595+meakaliaG@users.noreply.github.com>
2024-10-11 09:32:08 -0400

    initial

---
+++ favoriteNumbers/favoriteNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace favoriteNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
